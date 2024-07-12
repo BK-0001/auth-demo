c343b5003a023e4c2d696a2ebccf09ab942fd645
Bart Kang <bart.kang.0719@gmail.com>
2024-07-12 09:32:42 -0700

    setup layout and add sime header

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import expressEjsLayouts from "express-ejs-layouts";
 import path from "path";
 import { router as indexRouter } from "./routes/index.router";
 
@@ -7,5 +8,9 @@ export const app = express();
 // setup ejs
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// setup layout
+app.set("layout", path.join(__dirname, "views/layouts/layout"));
+app.use(expressEjsLayouts);
 
 app.use(indexRouter);
